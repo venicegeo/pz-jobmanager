4a10875b25520e317132deddf7c58287362343de
GitHubRGI <patrick.doody@rgi-corp.com>
2015-12-29 12:29:22 -0500

    Incorporate client factory

--- src/main/java/jobmanager/messaging/JobMessager.java
+++ src/main/java/jobmanager/messaging/JobMessager.java
@@ -1,14 +1,11 @@
 package main.java.jobmanager.messaging;
 
-import java.util.Properties;
-
 import javax.annotation.PostConstruct;
 
 import main.java.jobmanager.database.MongoAccessor;
+import messaging.job.KafkaClientFactory;
 
 import org.apache.kafka.clients.consumer.Consumer;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -34,47 +31,10 @@ public class JobMessager {
 	@PostConstruct
 	public void initialize() {
 		// Initialize the Consumer and Producer
-		initializeProducer();
-		initializeConsumer();
+		producer = KafkaClientFactory.getProducer(KAFKA_HOST, KAFKA_PORT);
+		consumer = KafkaClientFactory.getConsumer(KAFKA_HOST, KAFKA_PORT, KAFKA_GROUP);
 		// Start the runner that will relay Job Creation topics.
 		Thread createJobThread = new Thread(new CreateJobRunner(consumer, accessor));
 		createJobThread.start();
-	}
-
-	/**
-	 * Initializing the Kafka Producer that will relay messages.
-	 * 
-	 * TODO: Config please
-	 */
-	private void initializeProducer() {
-		// Initialize the Kafka Producer
-		Properties props = new Properties();
-		props.put("bootstrap.servers", String.format("%s:%s", KAFKA_HOST, KAFKA_PORT));
-		props.put("acks", "all");
-		props.put("retries", 0);
-		props.put("batch.size", 16384);
-		props.put("linger.ms", 1);
-		props.put("buffer.memory", 33554432);
-		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-
-		producer = new KafkaProducer<String, String>(props);
-	}
-
-	/**
-	 * Initializing the Kafka Consumer that will consume relayable messages.
-	 * 
-	 * TODO: Config please
-	 */
-	private void initializeConsumer() {
-		Properties props = new Properties();
-		props.put("bootstrap.servers", String.format("%s:%s", KAFKA_HOST, KAFKA_PORT));
-		props.put("group.id", KAFKA_GROUP);
-		props.put("enable.auto.commit", "true");
-		props.put("auto.commit.interval.ms", "1000");
-		props.put("session.timeout.ms", "30000");
-		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-		consumer = new KafkaConsumer<String, String>(props);
 	}
 }
