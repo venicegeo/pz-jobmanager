1622ca8e545d667f69bc8c7eec5cebda99cc2457
GitHubRGI <patrick.doody@rgi-corp.com>
2016-01-21 14:12:34 -0500

    Add CF/Jenkins scripts. Refactor app.properties location

---
+++ scripts/cf-deliver.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -ex
+
+pushd `dirname $0` > /dev/null
+base=$(pwd -P)
+popd > /dev/null
+
+# Gather some data about the repo
+source $base/vars.sh
+
+# Do we have this artifact in s3? 
+[ -f $base/../piazza-jobmanager*.jar ] || { aws s3 ls $S3URL && aws s3 cp $S3URL $base/../pz-jobmanager.jar || exit 1; }
