6d97319a491a8afad8cdcf57a9fe2c02a4d0bcd9
GitHubRGI <patrick.doody@rgi-corp.com>
2016-02-12 11:50:40 -0500

    Add job information endpoints for SAK admin service

--- src/main/java/jobmanager/controller/JobController.java
+++ src/main/java/jobmanager/controller/JobController.java
@@ -15,16 +15,22 @@
  **/
 package jobmanager.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jobmanager.database.MongoAccessor;
 import model.job.Job;
 import model.response.ErrorResponse;
 import model.response.JobStatusResponse;
 import model.response.PiazzaResponse;
+import model.status.StatusUpdate;
 
+import org.mongojack.DBQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.ResourceAccessException;
 
@@ -32,9 +38,13 @@ import org.springframework.web.client.ResourceAccessException;
 public class JobController {
 	@Autowired
 	private MongoAccessor accessor;
+	private static final String DEFAULT_PAGE_SIZE = "10";
+	private static final String DEFAULT_PAGE = "0";
 
 	/**
 	 * Returns the Job Status and potential Results of the specified Job ID.
+	 * This is used when the Gateway needs a synchronous, non-Kafka, response
+	 * for the specific status of a Job.
 	 * 
 	 * @param jobId
 	 *            The Job ID.
@@ -60,5 +70,99 @@ public class JobController {
 			exception.printStackTrace();
 			return new ErrorResponse(jobId, "Error fetching Job: " + exception.getMessage(), "Job Manager");
 		}
+	}
+
+	/**
+	 * Returns Jobs currently held by the Piazza Job table.
+	 * 
+	 * This is intended to be used by the Swiss-Army-Knife (SAK) administration
+	 * application for reporting the status of this Job Manager component. It is
+	 * not used in normal function of the Job Manager.
+	 * 
+	 * @param page
+	 *            The start page
+	 * @param pageSize
+	 *            The number of results per page
+	 * @return The List of all Jobs in the system.
+	 */
+	@RequestMapping(value = "/job", method = RequestMethod.GET)
+	public List<Job> getJobs(@RequestParam(value = "page", required = false, defaultValue = DEFAULT_PAGE) String page,
+			@RequestParam(value = "pageSize", required = false, defaultValue = DEFAULT_PAGE_SIZE) String pageSize) {
+		return accessor.getJobCollection().find().skip(Integer.parseInt(page) * Integer.parseInt(pageSize))
+				.limit(Integer.parseInt(pageSize)).toArray();
+	}
+
+	/**
+	 * Returns the Number of Jobs in the piazza system.
+	 * 
+	 * This is intended to be used by the Swiss-Army-Knife (SAK) administration
+	 * application for reporting the status of this Job Manager component. It is
+	 * not used in normal function of the Job Manager.
+	 * 
+	 * @return Number of Jobs in the system.
+	 */
+	@RequestMapping(value = "/job/count", method = RequestMethod.GET)
+	public long getJobCount() {
+		return accessor.getJobCollection().count();
+	}
+
+	/**
+	 * Gets all Job Status types that can be queried for by the system.
+	 * 
+	 * This is intended to be used by the Swiss-Army-Knife (SAK) administration
+	 * application for reporting the status of this Job Manager component. It is
+	 * not used in normal function of the Job Manager.
+	 * 
+	 * @return List of Job Status types that can be queried for in the system.
+	 */
+	@RequestMapping(value = "/job/status", method = RequestMethod.GET)
+	public List<String> getStatuses() {
+		List<String> statuses = new ArrayList<String>();
+		statuses.add(StatusUpdate.STATUS_CANCELLED);
+		statuses.add(StatusUpdate.STATUS_COMPLETE);
+		statuses.add(StatusUpdate.STATUS_ERROR);
+		statuses.add(StatusUpdate.STATUS_FAIL);
+		statuses.add(StatusUpdate.STATUS_PENDING);
+		statuses.add(StatusUpdate.STATUS_RUNNING);
+		statuses.add(StatusUpdate.STATUS_SUBMITTED);
+		statuses.add(StatusUpdate.STATUS_SUCCESS);
+		return statuses;
+	}
+
+	/**
+	 * Gets the count for all Jobs of the specified status.
+	 * 
+	 * This is intended to be used by the Swiss-Army-Knife (SAK) administration
+	 * application for reporting the status of this Job Manager component. It is
+	 * not used in normal function of the Job Manager.
+	 * 
+	 * @return List of Jobs that match the specified status.
+	 */
+	@RequestMapping(value = "/job/status/{status}/count", method = RequestMethod.GET)
+	public int getStatusCount(@PathVariable(value = "status") String status) {
+		return accessor.getJobCollection().find(DBQuery.is("status", status)).count();
+	}
+
+	/**
+	 * Gets all Jobs, filtered by their Status. This follows the same pagination
+	 * rules as the getJobs() request.
+	 * 
+	 * This is intended to be used by the Swiss-Army-Knife (SAK) administration
+	 * application for reporting the status of this Job Manager component. It is
+	 * not used in normal function of the Job Manager.
+	 * 
+	 * @param page
+	 *            The start page
+	 * @param pageSize
+	 *            The number of results per page
+	 * @return The list of all Jobs that match the specified Status
+	 */
+	@RequestMapping(value = "/job/status/{status}", method = RequestMethod.GET)
+	public List<Job> getJobsByStatus(
+			@RequestParam(value = "page", required = false, defaultValue = DEFAULT_PAGE) String page,
+			@RequestParam(value = "pageSize", required = false, defaultValue = DEFAULT_PAGE_SIZE) String pageSize,
+			@PathVariable(value = "status") String status) {
+		return accessor.getJobCollection().find(DBQuery.is("status", status))
+				.skip(Integer.parseInt(page) * Integer.parseInt(pageSize)).limit(Integer.parseInt(pageSize)).toArray();
 	}
 }
