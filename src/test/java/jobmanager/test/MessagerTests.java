ed840bffe4f90e401172d26257f4e6d295024fb7
go0ty <patrick.doody@rgi-corp.com>
2016-06-09 13:29:30 -0400

    Cleanup unused imports

--- src/test/java/jobmanager/test/MessagerTests.java
+++ src/test/java/jobmanager/test/MessagerTests.java
@@ -15,13 +15,7 @@
  **/
 package jobmanager.test;
 
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Matchers.isA;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 import jobmanager.database.MongoAccessor;
 import jobmanager.messaging.JobMessager;
 import jobmanager.messaging.handler.AbortJobHandler;
