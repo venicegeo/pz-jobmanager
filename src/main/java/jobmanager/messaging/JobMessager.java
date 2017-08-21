4e5fb5c9b0af18b86f06e6cf6a2ac8fb17058556
Patrick D <patrick.doody@rgi-corp.com>
2017-08-21 11:57:49 -0400

    Remove unused Using

--- src/main/java/jobmanager/messaging/JobMessager.java
+++ src/main/java/jobmanager/messaging/JobMessager.java
@@ -20,7 +20,6 @@ import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
